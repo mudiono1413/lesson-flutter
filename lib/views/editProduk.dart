09d7463daf63f5c05093e2155ac947336c5af424
mudiono1413 <mudiono1413@gmail.com>
2019-08-29 18:18:06 +0700

    delete produk

--- lib/views/editProduk.dart
+++ lib/views/editProduk.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart'
+as http;
 import 'package:lesson_flutter/modal/api.dart';
 import 'package:lesson_flutter/modal/produkModel.dart';
 
@@ -9,102 +10,103 @@ class EditProduk extends StatefulWidget {
   final ProdukModel model;
   final VoidCallback reload;
   EditProduk(this.model, this.reload);
-  
 
-  @override
-  _EditProdukState createState() => _EditProdukState();
+
+  @override _EditProdukState createState()=>_EditProdukState();
 }
 
-class _EditProdukState extends State<EditProduk> {
-  final _key = new GlobalKey<FormState>();
-  String namaProduk, harga,qty;
+class _EditProdukState extends State < EditProduk> {
+  final _key=new GlobalKey < FormState>();
+  String namaProduk,
+  harga,
+  qty;
 
-  TextEditingController txtNama, txtQty, txtHarga;
+  TextEditingController txtNama,
+  txtQty,
+  txtHarga;
 
-  setup(){
-    txtNama = TextEditingController(text: widget.model.namaProduk);
-    txtQty  = TextEditingController(text:  widget.model.qty);
-    txtHarga = TextEditingController(text: widget.model.harga);
+  setup() {
+    txtNama=TextEditingController(text: widget.model.namaProduk);
+    txtQty=TextEditingController(text: widget.model.qty);
+    txtHarga=TextEditingController(text: widget.model.harga);
 
   }
 
-  check(){
-    final form = _key.currentState;
+  check() {
+    final form=_key.currentState;
+
     if (form.validate()) {
       form.save();
       submit();
-    }else{
-
     }
+
+    else {}
   }
 
-  submit() async{
-      final response = await http.post(BaseUrl().editProduk, body:{
-          "namaProduk" : namaProduk,
-          "qty" : qty,
-          "harga" : harga,
-          "idProduk" : widget.model.id
-      });
-      final data = jsonDecode(response.body);
-      int value = data['value'];
-      String pesana = data['messege'];
-      if (value == 1) {
-        setState(() {
-          widget.reload;
-         Navigator.pop(context); 
-        });
-      }else{
-        print(pesana);
+  submit() async {
+    final response=await http.post(BaseUrl().editProduk, body: {
+        "namaProduk": namaProduk,
+        "qty": qty,
+        "harga": harga,
+        "idProduk": widget.model.id
       }
 
+    );
+    final data=jsonDecode(response.body);
+    int value=data['value'];
+    String pesana=data['messege'];
+
+    if (value==1) {
+      setState(() {
+          widget.reload;
+          Navigator.pop(context);
+        }
+
+      );
+    }
+
+    else {
+      print(pesana);
+    }
+
   }
-  @override
-  void initState() {
+
+  @override void initState() {
     // TODO: implement initState
     super.initState();
     setup();
   }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-     
-         body: Form(
-        key:  _key,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ListView(
-          children: <Widget>[
-            TextFormField(
-              controller: txtNama,
-                onSaved: (e)=>namaProduk = e,
-                  decoration: InputDecoration(
-                    labelText: "Nama Produk"
-                  ),
-            ),
-             TextFormField(
-               controller: txtQty,
-                onSaved: (e)=>qty = e,
-                  decoration: InputDecoration(
-                    labelText: "Quantity"
-                  ),
-            ),
-             TextFormField(
-               controller: txtQty,
-                onSaved: (e)=>harga = e,
-                  decoration: InputDecoration(
-                    labelText: "Harga"
-                  ),
-            ),
-            MaterialButton(
-                onPressed: (){
-                    check();
-                },
-                child: Text("Simpan"),
-            )
-          ],
-        ),
+
+  @override Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),
+
+      body: Form(key: _key,
+        child: Padding(padding: const EdgeInsets.all(16.0),
+          child: ListView(children: < Widget > [ TextFormField(controller: txtNama,
+              onSaved: (e)=> namaProduk=e,
+              decoration: InputDecoration(labelText: "Nama Produk"
               ),
+            ),
+            TextFormField(controller: txtQty,
+              onSaved: (e)=> qty=e,
+              decoration: InputDecoration(labelText: "Quantity"
+              ),
+            ),
+            TextFormField(controller: txtQty,
+              onSaved: (e)=> harga=e,
+              decoration: InputDecoration(labelText: "Harga"
+
+              ),
+            ),
+            MaterialButton(onPressed: () {
+                check();
+              }
+
+              ,
+              child: Text("Simpan"),
+            )],
+          ),
+        ),
       ),
     );
   }
--- lib/views/product.dart
+++ lib/views/product.dart
@@ -5,114 +5,173 @@ import 'package:lesson_flutter/modal/api.dart';
 import 'package:lesson_flutter/modal/produkModel.dart';
 import 'package:lesson_flutter/views/editProduk.dart';
 import 'package:lesson_flutter/views/tambahProduk.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart'
+as http;
 
 class Product extends StatefulWidget {
 
-  @override
-  _ProductState createState() => _ProductState();
+  @override _ProductState createState()=>_ProductState();
 }
 
-class _ProductState extends State<Product> {
-  var loading = false;
-  final list = new List<ProdukModel>();
-  final GlobalKey<RefreshIndicatorState> _refresh = GlobalKey<RefreshIndicatorState>();
-  Future<void> _lihatData() async{
-    list.clear();
-    setState(() {
-     loading = true; 
-    });
-    final response = await http.get(BaseUrl().lihatproduk);
-    if (response.contentLength == 2) {
-      
-    } else {
-      final data  = jsonDecode(response.body);
-      data.forEach((api){
+class _ProductState extends State < Product> {
+  var loading=false;
+  final list=new List < ProdukModel>();
+  final GlobalKey < RefreshIndicatorState>_refresh=GlobalKey < RefreshIndicatorState>();
 
-        final ab = new ProdukModel(
-          api['id'], 
-          api['namaProduk'], 
-          api['qty'], 
-          api['harga'], 
-          api['createdDate'],
-           api['idUsers'], 
-           api['nama']
-           );
-            list.add(ab);
-      });
+  Future < void>_lihatData() async {
+    list.clear();
+
+    setState(() {
+        loading=true;
+      }
+
+    );
+    final response=await http.get(BaseUrl().lihatproduk);
+
+    if (response.contentLength==2) {}
+
+    else {
+      final data=jsonDecode(response.body);
+
+      data.forEach((api) {
+
+          final ab=new ProdukModel(api['id'],
+            api['namaProduk'],
+            api['qty'],
+            api['harga'],
+            api['createdDate'],
+            api['idUsers'],
+            api['nama']);
+          list.add(ab);
+        }
+
+      );
+
       setState(() {
-       loading = false; 
-      });
-       }
+          loading=false;
+        }
+
+      );
+    }
   }
 
-  @override
-  void initState() {
+  _dialoddelete(String id) {
+
+    showDialog(context: context,
+      builder: (context) {
+        return Dialog(child: ListView(padding: EdgeInsets.all(16.0),
+            shrinkWrap: true,
+            children: <Widget>[ Text("Are you sure want to delete data ?",
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[ 
+              InkWell(
+                onTap: (){
+                  _delete(id);
+
+                },
+                child: Text("Yes")),
+              SizedBox(
+                  width: 16.0,
+              ),
+              InkWell(
+                onTap: (){
+                  Navigator.pop(context);
+                } ,
+                child: Text("No")
+                )
+              ],
+            ),
+            ],
+          ),
+        );
+      }
+
+    );
+  }
+
+  _delete(String id) async {
+    final response=await http.post(BaseUrl().deleteProduk, body: {
+        "idProduk": id
+      }
+
+    );
+    final data=jsonDecode(response.body);
+    int value=data['value'];
+    String pesan=data['messege'];
+
+    if (value==1) {
+      setState(() {
+          _lihatData();
+          Navigator.pop(context);
+        }
+
+      );
+    }
+
+    else {
+      print(pesan);
+    }
+  }
+
+  @override void initState() {
     // TODO: implement initState
     super.initState();
     _lihatData();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => TambahProduk(_lihatData)));
-          },
-          child: Icon(Icons.add),
+  @override Widget build(BuildContext context) {
+    return Scaffold(floatingActionButton: FloatingActionButton(onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> TambahProduk(_lihatData)));
+        }
 
-        ),
-      body: RefreshIndicator(
-        onRefresh: _lihatData,
+        ,
+        child: Icon(Icons.add),
+
+      ),
+      body: RefreshIndicator(onRefresh: _lihatData,
         key: _refresh,
-              child: loading ? Center(
-          child: CircularProgressIndicator()) :
-           ListView.builder(
-             itemCount: list.length,
-             itemBuilder: (context, i){
-               final x = list[i];
-               return Padding(
-                 padding: const EdgeInsets.all(10.0),
-                 child: Container(
-                   child: Row(
-                     children: <Widget>[
-                       Expanded(
-                                      child: Column(
-                       crossAxisAlignment: CrossAxisAlignment.start,
-                       children: <Widget>[
-                         Text(x.namaProduk,
-                           style: TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold),),
-                          Text(x.qty),
-                           Text(x.harga),
-                            Text(x.nama),
-                             Text(x.createdDate),
+        child: loading ? Center(child: CircularProgressIndicator()) : ListView.builder(itemCount: list.length,
+          itemBuilder: (context, i) {
+            final x=list[i];
+            return Padding(padding: const EdgeInsets.all(10.0),
+              child: Container(child: Row(children: < Widget > [ Expanded(child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                      children: < Widget > [ Text(x.namaProduk,
+                        style: TextStyle(fontSize: 18.0,
+                          fontWeight: FontWeight.bold), ),
+                      Text(x.qty),
+                      Text(x.harga),
+                      Text(x.nama),
+                      Text(x.createdDate),
 
-                       ],
-                     ),
-                       ),
-                       IconButton(
-                         onPressed: (){
-                           Navigator.of(context).push(MaterialPageRoute(
-                             builder: (context)=> EditProduk(x, _lihatData)));
-                         },
-                         icon: Icon(Icons.edit),
-                       ),
-                       IconButton(
-                         onPressed: (){},
-                         icon: Icon(Icons.delete),
-                       )
-                     ],
-                   ),
-                 ),
-               );
-             },
-           ),
-      )
-    
-    );
+                      ],
+                    ),
+                  ),
+                  IconButton(onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=> EditProduk(x, _lihatData)));
+                    }
+
+                    ,
+                    icon: Icon(Icons.edit),
+                  ),
+                  IconButton(onPressed: () {
+                      // _delete(x.id);
+                      _dialoddelete(x.id);
+                    }
+
+                    ,
+                    icon: Icon(Icons.delete),
+                  )],
+                ),
+              ),
+            );
+          }
+
+          ,
+        ),
+      ));
   }
 }
